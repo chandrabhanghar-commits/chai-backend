41805df0dad2303ee6300fcfaf810510cb38e1ac
Chandrabhan <chandrabhanghar@gmail.com>
2026-02-25 15:02:07 +0530

    Add refreshAccessToken endpoint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors"
+import cookieParser  from "cookie-parser"
 
 const app = express();
 
+app.use(cors({
+  origin:process.env.CORS_ORIGIN,
+  Credentials:true
+}))
+
 // middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({ extended: true, limit:"16kb" }));
 
 // test route
 app.get("/", (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import "dotenv/config";
+import 'dotenv/config';
 import connectDB from "./db/index.js";
 import app from "./app.js";
 
-connectDB();
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log("Server running on port", process.env.PORT);
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB db connection failed !!!", error);
+  }); // ← Yeh closing braces aur semicolon missing tha
